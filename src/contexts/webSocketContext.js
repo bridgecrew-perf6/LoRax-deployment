74dd4454c702e1181ba17b8cb06406599da43630
Matthew Nieva <matthewcn56@gmail.com>
2022-01-16 07:23:52 -0800

    allowed wss on https

--- src/contexts/webSocketContext.js
+++ src/contexts/webSocketContext.js
@@ -1,100 +1,101 @@
-import React, {useState, useEffect} from "react";
-const gateways= [process.env.REACT_APP_HARDWARE_GATEWAY_1, process.env.REACT_APP_HARDWARE_GATEWAY_2];
+import React, { useState, useEffect } from "react";
+const gateways = [
+  process.env.REACT_APP_HARDWARE_GATEWAY_1,
+  process.env.REACT_APP_HARDWARE_GATEWAY_2,
+];
 // const ws_gateways = gateways.map(entry => "ws://" + entry + "/ws" );
 
 export const WebSocketContext = React.createContext({
   state: {},
-  connection: undefined
+  connection: undefined,
 });
 
 //pass in gateway for cloud context with props
-export function WebSocketProvider(props){
-    const [connection, setConnection] = useState(undefined);
-    
-    const [state, setState] = useState({
-        soil: {
-          value: "-",
-          warning: "",
-        },
-        temp: {
-          value: "-",
-          warning: "",
-        },
-        humid: {
-          value: "-",
-          warning: "",
-        },
-        uv: {
-          value: "-",
-          warning: "",
-        },
+export function WebSocketProvider(props) {
+  const [connection, setConnection] = useState(undefined);
+
+  const [state, setState] = useState({
+    soil: {
+      value: "-",
+      warning: "",
+    },
+    temp: {
+      value: "-",
+      warning: "",
+    },
+    humid: {
+      value: "-",
+      warning: "",
+    },
+    uv: {
+      value: "-",
+      warning: "",
+    },
+  });
+
+  //useEffect for initializing websocket
+  useEffect(() => {
+    const newConnection = new WebSocket("wss://firerisk.herokuapp.com/ws");
+
+    newConnection.onopen = () => {
+      let data = JSON.stringify({
+        type: 0, // registering
+        from: "client",
       });
+      newConnection.send(data);
+    };
 
-      //useEffect for initializing websocket
-    useEffect(() => {
-        const newConnection = new WebSocket("ws://firerisk.herokuapp.com/ws");
-    
-        newConnection.onopen = () => {
-          let data = JSON.stringify(
-            {
-              "type": 0, // registering
-              "from": "client",
-            }
-          );
-          newConnection.send(data);
-        }
-        
-        newConnection.onmessage = (msg) => {
-          const { type, sensor, value } = JSON.parse(msg.data);
-          if (type === 1) {
-            console.log("previous state");
-            console.log(state);
-            console.log(sensor);
-            let newState = {...state};
-            newState[sensor].value = value;
-            newState[sensor].warning = getValueWarning({ sensor, value});
-            setState(newState);
-            console.log("new state");
-            console.log(state);
-          }
-        }
-        
-        setConnection(newConnection);
-        return () => newConnection.close();
-      }, []);
+    newConnection.onmessage = (msg) => {
+      const { type, sensor, value } = JSON.parse(msg.data);
+      if (type === 1) {
+        console.log("previous state");
+        console.log(state);
+        console.log(sensor);
+        let newState = { ...state };
+        newState[sensor].value = value;
+        newState[sensor].warning = getValueWarning({ sensor, value });
+        setState(newState);
+        console.log("new state");
+        console.log(state);
+      }
+    };
 
-    //websocket functions
+    setConnection(newConnection);
+    return () => newConnection.close();
+  }, []);
 
+  //websocket functions
 
-    return(
-        <WebSocketContext.Provider 
-            value = {{
-                connection,
-                state,
-            }}>
-            {props.children}
-        </WebSocketContext.Provider>
-    );
+  return (
+    <WebSocketContext.Provider
+      value={{
+        connection,
+        state,
+      }}
+    >
+      {props.children}
+    </WebSocketContext.Provider>
+  );
 }
 
 const valueWarnings = {
-  soil: v => v >= 30 && v <= 70,
-  temp: v => v >= 50 && v <= 80,
-  humid: v => v >= 30 && v <= 80,
-  uv: v => v === "Very Low" || v === "Very High",
-}
+  soil: (v) => v >= 30 && v <= 70,
+  temp: (v) => v >= 50 && v <= 80,
+  humid: (v) => v >= 30 && v <= 80,
+  uv: (v) => v === "Very Low" || v === "Very High",
+};
 
 function getValueWarning({ sensor, value }) {
-    const isDangerousValue = valueWarnings[sensor];
-  
-    if (isDangerousValue(value)) return `The ${sensorToFullName[sensor]} is unsafe.`;
-    else return '';
-  }
-  
-  const sensorToFullName = {
-    soil: "soil",
-    temp: "temperature",
-    humid: "humidity",
-    uv: "UV exposure"
-  }
-  
+  const isDangerousValue = valueWarnings[sensor];
+
+  if (isDangerousValue(value))
+    return `The ${sensorToFullName[sensor]} is unsafe.`;
+  else return "";
+}
+
+const sensorToFullName = {
+  soil: "soil",
+  temp: "temperature",
+  humid: "humidity",
+  uv: "UV exposure",
+};
